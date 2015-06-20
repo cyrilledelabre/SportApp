e5ebe1da58fdc16b73c85623a2be848f7a978816
cyrilledelabre <cyrilledelabre@gmail.com>
2015-06-20 09:51:44 -0300

    Removed mylogfile.log

--- app/src/main/java/com/cyrilledelabre/riosportapp/utils/Maps/SphericalUtil.java
+++ app/src/main/java/com/cyrilledelabre/riosportapp/utils/Maps/SphericalUtil.java
@@ -40,7 +40,7 @@ public class SphericalUtil {
      * in the specified heading (expressed in degrees clockwise from north).
      *
      * @param from     The LatLng from which to start.
-     * @param distance The distance to travel.
+     * @param distance The distance to travel in meters
      * @param heading  The heading in degrees clockwise from north.
      */
     public static LatLng computeOffset(LatLng from, double distance, double heading) {
